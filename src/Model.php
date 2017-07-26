f21a8d4e6dd7671867da916ea46d15e6ce260df0
Zcasper <testuser@mail.linpax.org>
2017-07-26 21:43:54 +0700

    init

---
+++ src/Model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @link https://github.com/linpax/microphp-framework
+ * @copyright Copyright &copy; 2013 Oleg Lunegov
+ * @license https://github.com/linpax/microphp-framework/blob/master/LICENSE
+ */
+
+namespace Micro\Mvc;
+
+
+interface Model
+{
+
+}
